452eb6e2d50731ea78c9bd5edf52decbe63f4cc5
Daniel <angus9209@gmail.com>
2022-06-09 01:35:59 +1200

    pass state using through router to detail page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
 
-import { useState,useEffect } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Seo from '../components/Seo'
 
 
 export default function Home({results}){
- 
+ const router = useRouter();
+ const onClick = (id, title) => {
+  router.push(
+    `/movies/${title}/${id}` 
+  )
+ }
   return (
     <>
     <div className="container">
     <Seo title="Home"/>
-      {results?.map(movie => <div className="movie" key={movie.id}>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-        <h4>{movie.original_title}</h4>
-      </div>)}
+      {results?.map(movie =>
+            <div onClick={()=> onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>  
+              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+              <h4> 
+                <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+                  <a>{movie.original_title} </a>
+                </Link>
+              </h4>
+            </div>
+      )}
     </div>
     <style jsx>{`
         .container {
@@ -44,10 +56,10 @@ export default function Home({results}){
 }
 
 export async function getServerSideProps(){
-  const {results} = await (await fetch('http://localhost:3000/api/movies')).json();
+  const { results } = await (await fetch("http://localhost:3000/api/movies")).json();
   return {
     props: {
-      results, 
+      results
     }
   }
 
